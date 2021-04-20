7e04fd46c5ed802c48fad6148031a56f20c9e46d
esroleo <esroleo@gmail.com>
2021-04-20 16:14:41 -0400

    images are rendered now

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-//Added
-/*
-You need to import React in every component file.
-You can also import any images or CSS you want to use at the top.
-*/
 import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
-import Gallery from "./components/Gallery";
-//Added
-//import logo from './logo.svg';
-import './App.css';
+import Gallery from './components/Gallery';
 
-// starter App() js format.
 function App() {
-
-  // this was moved to parent for props to re-render
   const [categories] = useState([
     {
       name: 'commercial',
@@ -35,35 +24,11 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
-        <div>
-          <Gallery></Gallery>
-          <About></About>
-        </div>
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
       </main>
     </div>
   );
 }
-
-// function App() {
-//   return (
-//     //jsx code
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
