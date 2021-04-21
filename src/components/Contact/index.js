48b784e21fa41cf91aab5d69a524e1ef06fbb11d
esroleo <esroleo@gmail.com>
2021-04-20 22:43:45 -0400

    validation almost completed for the contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 
 
@@ -8,25 +9,53 @@ function ContactForm() {
     //const [formState, setFormState] = useState();
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
 
+    // hook to validate error message of the form
+    const [errorMessage, setErrorMessage] = useState('');
+    
     // deconstruct formState
     const { name, email, message } = formState;
 
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!errorMessage) {
+          setFormState({ [e.target.name]: e.target.value });
+          console.log('Form', formState);
+        }
+      };
+
+      
     // JSX
     // handle each target name = in one liner
     // before setFormState({...formState, name: e.target.value })
     // after setFormState({...formState, [e.target.name]: e.target.value })
-    function handleChange(e) {
-        setFormState({...formState, [e.target.name]: e.target.value })
-      }
+    const handleChange = (e) => {
+        if (e.target.name === 'email') {
+          const isValid = validateEmail(e.target.value);
+          if (!isValid) {
+            setErrorMessage('Your email is invalid.');
+          } else {
+            setErrorMessage('');
+          }
+        } else {
+          if (!e.target.value.length) {
+            setErrorMessage(`${e.target.name} is required.`);
+          } else {
+            setErrorMessage('');
+          }
+        }
+        //console.log('errorMessage', errorMessage); 
+        // if form data has passed then assigned correct form state
+        if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+          }
+    };
       
       //console.log(formState);
 
 
-    // Function to submit data
-    function handleSubmit(e) {
-    e.preventDefault();
-    console.log(formState);
-    }
+
 
     return (
         <section>
